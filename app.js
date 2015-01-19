3bc60782657bc21a3bde8f77a966fbb15148aa41
roubuntu <correo.rodrigo@gmail.com>
2015-01-19 13:02:09 -0600

    site map added

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var api = require('./routes/api');
+var apiContribuyentes = require('./routes/api-contribuyentes');
+
 
 var app = express();
 
@@ -26,6 +28,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/api', api); 
+app.use('/api/contribuyentes', apiContribuyentes);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/api-contribuyentes.js
+++ routes/api-contribuyentes.js
@@ -1,12 +1,14 @@
 'use strict';
-const
+const nconf = require('nconf'),
     siteMap = require('./../modules/api-site-map.json'),
     express = require('express'),
      router = express.Router();
 
+nconf.file('settings.json').env();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {  
-  res.status(200).json(siteMap);
+  res.status(200).json(siteMap.contribuyentes);
 });
 
 module.exports = router;
