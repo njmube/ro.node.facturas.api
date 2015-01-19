60b71b2524b93d615b637ccb7756d63a3d95bc96
roubuntu <correo.rodrigo@gmail.com>
2015-01-19 13:52:08 -0600

    added post method

--- routes/api-contribuyentes.js
+++ routes/api-contribuyentes.js
@@ -1,7 +1,10 @@
 'use strict';
 const nconf = require('nconf'),
     siteMap = require('./../modules/api-site-map.json'),
+     modelo = require('./../modules/contribuyente.json'),
     express = require('express'),
+    request = require('request'),
+          Q = require('q'),
      router = express.Router();
 
 nconf.file('settings.json').env();
@@ -10,5 +13,46 @@ nconf.file('settings.json').env();
 router.get('/', function(req, res, next) {  
   res.status(200).json(siteMap.contribuyentes);
 });
+
+router.get('/buscar/:rfc', function(req, res, next){
+  res.status(200).json({});
+});
+
+router.post('/crear/', function(req, res, next){  
+  let deferred = Q.defer();
+  request.post({
+    url: nconf("database"),
+    json: req.body
+  }, function(err, couchRes, body) {
+    if (err) {
+      deferred.reject(err);
+    } else {
+      deferred.resolve([couchRes, body]);
+    }
+  });
+
+  deferred.promise.then(function(args) {
+    let couchRes = args[0], body = args[1];
+    res.status(couchRes.statusCode).json(body);
+  }, function(err) {
+    res.status(502).json({ error: "bad_gateway", reason: err.code });
+  });
+  
+});
+
+router.get('/ver/:id', function(req, res, next){
+  res.status(200).json({});
+});
+
+router.delete('/borrar/:id', function(req, res, next){
+  res.status(200).json({});
+});
+
+router.get('/modelo/', function(req, res, next){
+  res.status(200).json(modelo);
+});
+
+
+
 
 module.exports = router;
